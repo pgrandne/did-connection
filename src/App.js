bb3480c340a1a8de88ad804852e92d16242f78c8
pgrandne <perrin.grandne@protonmail.com>
2022-02-16 00:38:17 +0100

    creation of the authentification button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { CeramicClient } from "@ceramicnetwork/http-client";
+import { getResolver as getKeyResolver } from "key-did-resolver";
+import { getResolver as get3IDResolver } from "@ceramicnetwork/3id-did-resolver";
+import { DID } from "dids";
+import { EthereumAuthProvider, ThreeIdConnect } from "@3id/connect";
 
-function App() {
+export default function App() {
+  // Create a ThreeIdConnect connect instance as soon as possible in your app to start loading assets
+  const threeID = new ThreeIdConnect();
+  const CERAMIC_URL = 'https://localhost:7007';
+
+  async function authenticateWithEthereum(ethereumProvider) {
+    // Request accounts from the Ethereum provider
+    const accounts = await ethereumProvider.request({
+      method: "eth_requestAccounts"
+    });
+    // Create an EthereumAuthProvider using the Ethereum provider and requested account
+    const authProvider = new EthereumAuthProvider(
+      ethereumProvider,
+      accounts[0]
+    );
+    // Connect the created EthereumAuthProvider to the 3ID Connect instance so it can be used to
+    // generate the authentication secret
+    await threeID.connect(authProvider);
+
+
+    const ceramic = new CeramicClient();
+    const did = new DID({
+      // Get the DID provider from the 3ID Connect instance
+      provider: threeID.getDidProvider(),
+      resolver: {
+        ...get3IDResolver(ceramic),
+        ...getKeyResolver()
+      }
+    });
+
+    // Authenticate the DID using the 3ID provider from 3ID Connect, this will trigger the
+    // authentication flow using 3ID Connect and the Ethereum provider
+    await did.authenticate();
+
+    // The Ceramic client can create and update streams using the authenticated DID
+    ceramic.did = did;
+  }
+
+  // When using extensions such as MetaMask, an Ethereum provider may be injected as `window.ethereum`
+  async function tryAuthenticate() {
+    if (window.ethereum == null) {
+      throw new Error("No injected Ethereum provider");
+    }
+    await authenticateWithEthereum(window.ethereum);
+    console.log('coucou');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test de connection</h1>
+      <button onClick={tryAuthenticate}>se connecter</button>
     </div>
   );
 }
-
-export default App;
